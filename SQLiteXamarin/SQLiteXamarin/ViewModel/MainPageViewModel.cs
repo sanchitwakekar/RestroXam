3373e97421b72f383a87c6b446381b7cc9ee7805
sanchit.wakekar <sanchit.manoj.wakekar@gmail.com>
2019-12-23 17:02:26 +0530

    Updated Structure

--- SQLiteXamarin/SQLiteXamarin/ViewModel/MainPageViewModel.cs
+++ SQLiteXamarin/SQLiteXamarin/ViewModel/MainPageViewModel.cs
@@ -6,9 +6,34 @@ namespace SQLiteXamarin.ViewModel
 {
     class MainPageViewModel
     {
+        private string _username,_password,_role;
+        public List<string> Role = new List<string>() { "Customer", "Owner" };
+
         public MainPageViewModel()
         {
-
+           
+        }
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value;
+            }
         }
     }
 }
