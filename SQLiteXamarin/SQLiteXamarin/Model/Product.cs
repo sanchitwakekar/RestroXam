9941191fe30b929749f0791b83fcd023f197d6f7
Sanchit Manoj Wakekar <sanchit.wakekar@synapse.com>
2019-12-23 16:27:46 +0530

    Add project files.

---
+++ SQLiteXamarin/SQLiteXamarin/Model/Product.cs
@@ -0,0 +1,18 @@
+﻿using SQLiteNetExtensions.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLiteXamarin.Model
+{
+    class Product
+    {
+        public int pro_id { get; set; }
+        public string pro_name { get; set; }
+        public string pro_price { get; set; }
+        [ForeignKey(typeof(ProductCategory))]
+        public string pro_category_id { get; set; }
+        [OneToMany]     
+        public ProductCategory productCategory { get; set; }
+    }
+}
---
+++ SQLiteXamarin/SQLiteXamarin/Model/ProductCategory.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLiteXamarin.Model
+{
+    class ProductCategory
+    {
+        [PrimaryKey, AutoIncrement]
+        public int cat_id { get; set; }
+        public string cat_name { get; set; }
+    }
+}
