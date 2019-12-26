350e6f58941273ec800582d146e71471fc56e884
Harshal.jaiswal <harshaljaiswal25@gmail.com>
2019-12-26 15:34:12 +0530

    updated restaurant

--- SQLiteXamarin/SQLiteXamarin/View/OwnerView.xaml.cs
+++ SQLiteXamarin/SQLiteXamarin/View/OwnerView.xaml.cs
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 using SQLiteXamarin.Model;
 using SQLiteXamarin.ViewModel;
-=======
-﻿using SQLiteXamarin.Model;
->>>>>>> a5889fc49609c89d433758fb97e8e94494eefe84
 using System;
 using System.Collections.Generic;
 using System.Linq;
