a55664f854eaf51a3f329786e5e36b9324683bb4
sanchit.wakekar <sanchit.manoj.wakekar@gmail.com>
2019-12-31 11:35:48 +0530

    Cart Model added

---
+++ SQLiteXamarin/SQLiteXamarin/Model/Cart.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLiteXamarin.Model
+{
+    public class Cart
+    {
+        public int user_id { get; set; }
+    }
+}
